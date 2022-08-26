97da6e932e9196d873461742f87bc517f2687352
h.bence <bence2118@gmail.com>
2022-08-26 20:23:44 +0200

    added-dynamic-datas

--- js/app.js
+++ js/app.js
@@ -1,6 +1,13 @@
 const app = new Vue({
     el: '#app',
     data:{
-  
+        irstName: '',
+        lastName: '',
+        email: '',
+        ticketQuantity: 1,
+        ticketType: 'general',
+        referrals: [],
+        specialRequests: '',
+        purchaseAgreementSigned: false,
     }
   });
