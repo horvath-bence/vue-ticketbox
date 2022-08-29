838f258e97c288539fc5ffd1b0aa8ff019f58a3c
horvath-bence <57896606+horvath-bence@users.noreply.github.com>
2022-08-29 11:35:34 +0200

    added-propertys-to-email-validation

--- js/app.js
+++ js/app.js
@@ -8,7 +8,8 @@ const app = new Vue({
     ticketType: 'general',
     referrals: [],
     specialRequests: '',
-    purchaseAgreementSigned: false
+    purchaseAgreementSigned: false,
+    requiredFieldClass: 'required'
   },
   computed: {
     fullName: {
@@ -50,7 +51,13 @@ const app = new Vue({
       return this.email.includes('@');
     },
     formIsValid: function() {
-      return this.firstName && this.lastName && this.email && this.purchaseAgreementSigned;
+      return this.firstName && this.lastName && this.emailIsValid && this.purchaseAgreementSigned;
+    },
+    emailClasses: function() {
+      return {
+      touched: this.email.length !== 0,
+      invalid: this.email && !this.emailIsValid
+      };
     }
   },
   watch: {
